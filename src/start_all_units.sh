1e8b26091f02212b067198f5d0c89093c6da7131
frontbear <frontbear@outlook.com>
2018-02-21 21:46:51 +0800

    bugfix

--- src/start_all_units.sh
+++ src/start_all_units.sh
@@ -7,22 +7,31 @@ chown -R user7:group7    /usr/local/php
 chown -R user7:group7    /usr/local/mysql
 chown -R user7:group7    /usr/local/redis
 
-# init mysql
 if [ -z "$(ls -A /usr/local/mysql/datadir)" ]; then
-   /usr/local/mysql/bin/mysqld --defaults-file=/etc/my.cnf --initialize-insecure --user=mysql --basedir=/usr/local/mysql --datadir=/usr/local/mysql/datadir
+   /usr/local/mysql/bin/mysqld --defaults-file=/etc/my.cnf --initialize-insecure --user=user7 --basedir=/usr/local/mysql --datadir=/usr/local/mysql/datadir
    sleep 3s
 fi
 
-# start mysql
+# mysql
+if [ ! -f /usr/local/mysql/log_error_file.log ]; then
+    touch /usr/local/mysql/log_error_file.log
+    chown -R user7:group7    /usr/local/mysql
+fi
+
 /usr/local/mysql/support-files/mysql.server start
 sleep 3s
 chmod 777 /tmp/mysql.sock
 
-# start php-fpm
+# php
 /usr/local/php/sbin/php-fpm
 
-# start redis-server
+# redis
 /usr/local/redis/bin/redis-server /usr/local/redis/conf_file.conf
 
-# start nginx
+# nginx
 /usr/local/nginx/sbin/nginx
+
+chown -R user7:group7    /usr/local/nginx
+chown -R user7:group7    /usr/local/php
+chown -R user7:group7    /usr/local/mysql
+chown -R user7:group7    /usr/local/redis
