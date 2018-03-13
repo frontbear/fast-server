53c0b26291076e530ddd6a0085b4a2950d3c26dd
frontbear <frontbear@outlook.com>
2018-03-13 12:32:33 +0800

    php-nginx

---
+++ src/nginx-php/start_service.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eux
+
+chown -R user7:group7    /usr/local/nginx
+chown -R user7:group7    /usr/local/php
+
+/usr/local/php/sbin/php-fpm
+/usr/local/nginx/sbin/nginx
+
+chown -R user7:group7    /usr/local/nginx
+chown -R user7:group7    /usr/local/php
